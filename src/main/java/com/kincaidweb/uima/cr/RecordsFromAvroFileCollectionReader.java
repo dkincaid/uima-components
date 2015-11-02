2879cae0ff914918aec52e3f1198d1fa914e09a4
Dave Kincaid <kincaid.dave@gmail.com>
2015-11-01 20:02:30 -0600

    Various build fixes

--- src/main/java/com/kincaidweb/uima/cr/RecordsFromAvroFileCollectionReader.java
+++ src/main/java/com/kincaidweb/uima/cr/RecordsFromAvroFileCollectionReader.java
@@ -5,10 +5,9 @@ import org.apache.avro.file.FileReader;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.log4j.Logger;
-import org.apache.uima.cas.CAS;
-import org.apache.uima.cas.CASException;
+import org.apache.uima.UimaContext;
 import org.apache.uima.collection.CollectionException;
-import org.apache.uima.collection.CollectionReader_ImplBase;
+import org.apache.uima.fit.component.JCasCollectionReader_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
@@ -20,8 +19,15 @@ import java.io.IOException;
 
 /**
  * UIMA Collection reader that reads records from an Avro file.
+ *
+ * Parameters:
+ * <ul>
+ *     <li>InputFileName: full path to the Avro file to be read.</li>
+ *     <li>DocumentIdField: (not currently used)</li>
+ *     <li>ContentField: the name of the field in the schema that holds the text of the document</li>
+ * </ul>
  */
-public class RecordsFromAvroFileCollectionReader extends CollectionReader_ImplBase {
+public class RecordsFromAvroFileCollectionReader extends JCasCollectionReader_ImplBase {
     private Logger logger = Logger.getLogger(this.getClass().getName());
 
     public static final String PARAM_INPUT_FILE_NAME = "InputFileName";
@@ -41,7 +47,7 @@ public class RecordsFromAvroFileCollectionReader extends CollectionReader_ImplBa
     private int recordsRead;
 
     @Override
-    public void initialize() throws ResourceInitializationException {
+    public void initialize(UimaContext context) throws ResourceInitializationException {
         String inputFileName = (String) getConfigParameterValue(PARAM_INPUT_FILE_NAME);
         documentIdField = (String) getConfigParameterValue(PARAM_DOCUMENT_ID_FIELD);
         contentField = (String) getConfigParameterValue(PARAM_CONTENT_FIELD);
@@ -54,10 +60,7 @@ public class RecordsFromAvroFileCollectionReader extends CollectionReader_ImplBa
     }
 
     @Override
-    public void getNext(CAS cas) throws IOException, CollectionException {
-        try {
-            JCas jcas = cas.getJCas();
-
+    public void getNext(JCas jcas) throws IOException, CollectionException {
             GenericRecord nextRecord = reader.next();
             String documentId = String.valueOf(nextRecord.get(documentIdField));
             String content = String.valueOf(nextRecord.get(contentField));
@@ -68,11 +71,6 @@ public class RecordsFromAvroFileCollectionReader extends CollectionReader_ImplBa
             //docId.addToIndexes();
 
             recordsRead++;
-
-        } catch (CASException e) {
-            logger.error("Unable to open the CAS provided!", e);
-
-        }
     }
 
     @Override
